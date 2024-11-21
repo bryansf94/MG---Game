e17587d36eeabc2f3e7a6071b82b9528246979c2
Brayan Henrique <90359278+bryansf94@users.noreply.github.com>
2024-11-21 05:25:55 -0300

    subindo inicio do script de iniciar game

---
+++ src/rodar_game.js
@@ -0,0 +1,11 @@
+/* criando variavel que irá receber o clique do botão para iniciar o game na página inde html*/
+
+const $BotaoComeçarGame = document.querySelector(".place-holder")
+/*passando a variavel na função de iniciar o game */
+
+$BotaoComeçarGame.addEvenListener("click", startGame)
+/* iniciando a função de começar game */
+
+function startGame() {
+
+}
