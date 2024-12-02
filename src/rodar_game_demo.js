bbbd104dd89a96d01f5da7f318e4bd285a2f5f72
bryansf94 <bryansf94@gmail.com>
2024-12-02 05:33:13 -0300

    versão 1.0 do Java js de rodar game

--- src/rodar_game_demo.js
+++ src/rodar_game_demo.js
@@ -2,27 +2,39 @@
 const $BotaoComeçarGame = document.querySelector(".btn_Iniciargame")
 /* criando variavel que irá receber a div das questões da página Rodar_Game*/
 const $questionsConnatianer = document.querySelector(".place.holder")
-/*passando a variavel na função de iniciar o game */
+// vvariavel com o botão de proxima pergunta
+const $nextQuestion = document.querySelector(".next-question")
+const $questionText = document.querySelector(".questions")
+const $answersContainer = document.querySelector(".answers-container")
+const $answers = document.querySelectorAll(".answer")
 
+/*passando a variavel na função de iniciar o game */
 $BotaoComeçarGame.addEvenListener("click", startGame)
 /* iniciando a função de começar game */
-
+$nextQuestion.addEvenListener("click", displayNextQuestions)
 let currentQuestionsIndexx = 0;
+
 
 function startGame() {
 /* Place Holder, ainda trabaalhando nisso */
 displayNextQuestions()
 }
 
-function displayNextQuestions(){
-while($answerConatiner.firstChild) {
-    $answerConatiner.removeChild($answerConatiner.firstChil)
-}
+function displayNextQuestions() {
+    resetState()
+
+    if (questions === currentQuestionsIndexx){
+        return finishGame()
+    }
+
+//remevendo atributos do body
+document.body.removeAttribute("class")
+
 // Define o texto da pergunta atual
 $questionText.textContent = questions[currentQuestionIndex].question
 
 // Itera sobre cada resposta disponível para a pergunta atual
-questions[currentQuestionIndex].answers.forEach(answer => {
+questions[currentQuestionsIndexx].answers.forEach(answer => {
     // Cria um novo botão para a resposta
     const newAsnwer = document.createElement("button")
     
@@ -44,6 +56,14 @@ questions[currentQuestionIndex].answers.forEach(answer => {
     // a função 'selectAnswer' seja executada
     newAsnwer.addEventListener("click", selectAnswer)
 })
+
+function resetState(){
+    while($answerConatiner.firstChild) {
+        $answerConatiner.removeChild($answerConatiner.firstChil)
+    }
+}
+
+}
 /* função que captura o evento da resposta selecionada pelo usuário e então confere se ela possui o atributo "correct" */
 
 function selectAnswer(event) {
@@ -57,11 +77,36 @@ if(answerClicked.dataset.correct) {
 }
 document.querySelectorAll(".answer").forEach(button => {
     if (button.dataset.correct){
+        button.body.classList.add("correct")
 
+    } else {
+        button.body.classList.add("correct")
     }
-})
-}
 
+    button.disable = true
+})
+$nextQuestion.classList.remove("hide")
+currentQuestionsIndexx++
+}
+function finishGame() {
+    const totalQuestions = questions.length
+    const performance = Math.floor(totalCorrect * 100 / totalQuestions)
+    
+    let message = ""
+  
+    switch (true) {
+      case (performance >= 90):
+        message = "Excelente :)"
+        break
+      case (performance >= 70):
+        message = "Muito bom :)"
+        break
+      case (performance >= 50):
+        message = "Bom"
+        break
+      default:
+        message = "Pode melhorar :("
+    }
 
 
 
