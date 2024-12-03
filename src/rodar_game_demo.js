5b5d0531549aca47b79d02710effd35ac553ad7c
Brayan Henrique de Souza Rodrigues <brayan.rodrigues@delp.com.br>
2024-12-03 05:50:38 -0300

    Subindo Fixed do código / subindo perguntas atualizadas

--- src/rodar_game_demo.js
+++ src/rodar_game_demo.js
@@ -15,13 +15,8 @@ let totalCorrect = 0;
 
 // Função para iniciar o jogo
 function startGame() {
-<<<<<<< HEAD
-    
-    $questionsContainer.classList.remove("hide");
-=======
     $botaoComecarGame.classList.add("hide")
     $questionsContainer.classList.remove("hide")
->>>>>>> c16fba3e00cbe322fc536ed4c6e0264f15eb77a4
     displayNextQuestions();
     $botaoComecarGame.classList.add("hide"); 
 }
@@ -87,16 +82,9 @@ function selectAnswer(event) {
 
 // Função para finalizar o jogo
 function finishGame() {
-<<<<<<< HEAD
-    const totalQuestions = questions.length;
-    const performance = Math.floor((totalCorrect * 100) / totalQuestions);
-    $nextQuestion.classList.add("hide"); 
-    $questionText.classList.add("hide"); 
-=======
     const totalQuestions = questions.length * 100;
     const performance = totalCorrect;
 
->>>>>>> c16fba3e00cbe322fc536ed4c6e0264f15eb77a4
 
     let message;
     if (performance >= 900) {
@@ -115,16 +103,7 @@ function finishGame() {
     `
         <p class="final-message">Você pontuou ${totalCorrect} de ${totalQuestions} pontos possíveis!</p>
 
-<<<<<<< HEAD
-        <span>
-            Resultado: <br> 
-            Player 1 <br>
-            Player 2 <br>
-            Player 3
-        </span>
-=======
         <span>Obrigado por ter participado! </span>
->>>>>>> c16fba3e00cbe322fc536ed4c6e0264f15eb77a4
 
         <button onclick=window.location.reload() class="button"> Jogar novamente </button>
     `
@@ -132,83 +111,74 @@ function finishGame() {
 
 // Banco de perguntas e respostas
 const questions = [
-    {
-      question: "Dentro de qual elemento HTML colocamos o JavaScript?",
-      answers: [
-        { text: "<javascript>", correct: false },
-        { text: "<js>", correct: false },
-        { text: "<script>", correct: true },
-        { text: "<scripting>", correct: false }
-      ]
-    },
-    {
-      question: "Onde é o lugar correto para inserir JavaScript?",
-      answers: [
-        { text: "Tanto no <head> quanto no <body> está correto", correct: true },
-        { text: "No <body>", correct: false },
-        { text: "No <head>", correct: false },
-        { text: "Em outro lugar", correct: false }
-      ]
-    },
-    {
-      question: 'Qual é a sintaxe correta para se referir a um script externo chamado "xxx.js"',
-      answers: [
-        { text: '<script src="xxx.js">', correct: true },
-        { text: '<script href="xxx.js">', correct: false },
-        { text: '<script name="xxx.js">', correct: false },
-        { text: "Nenhuma das alternativas", correct: false }
-      ]
-    },
-    {
-      question: 'O arquivo JavaScript externo deve conter a tag <script>',
-      answers: [
-        { text: "Verdadeiro", correct: false },
-        { text: "Falso", correct: true }
-      ]
-    },
-    {
-      question: 'Como escrever "Hello World" numa caixa de alerta?',
-      answers: [
-        { text: 'msg("Hello World");', correct: false },
-        { text: 'alert("Hello World");', correct: true },
-        { text: 'msgBox("Hello World");', correct: false },
-        { text: 'alertBox("Hello World");', correct: false }
-      ]
-    },
-    {
-      question: 'Como podemos criar uma função no JavaScript?',
-      answers: [
-        { text: 'function:myFunction()', correct: false },
-        { text: 'function myFunction()', correct: true },
-        { text: 'function = myFunction()', correct: false },
-        { text: 'Nenhum desses códigos criaria uma função', correct: false }
-      ]
-    },
-    {
-      question: 'Como podemos chamar uma função chamada "minhaFuncao"?',
-      answers: [
-        { text: 'call minhaFuncao()', correct: false },
-        { text: 'call function minhaFuncao()', correct: false },
-        { text: 'Nenhum desses códigos chamaria essa função', correct: false },
-        { text: 'minhaFuncao()', correct: true },
-      ]
-    },
-    {
-        question: 'Como podemos chamar uma função chamada "minhaFuncao"?',
-        answers: [
-          { text: 'call minhaFuncao()', correct: false },
-          { text: 'call function minhaFuncao()', correct: false },
-          { text: 'Nenhum desses códigos chamaria essa função', correct: false },
-          { text: 'minhaFuncao()', correct: true },
-        ]
-      },
-      {
-        question: 'Como podemos chamar uma função chamada "minhaFuncao"?',
-        answers: [
-          { text: 'call minhaFuncao()', correct: false },
-          { text: 'call function minhaFuncao()', correct: false },
-          { text: 'Nenhum desses códigos chamaria essa função', correct: false },
-          { text: 'minhaFuncao()', correct: true },
-        ]
-      },
-  ]
+  {
+    question: "A primeira linguagem de programação de alto nível foi o Fortran, desenvolvida nos anos 1950.",
+    answers: [
+      { text: "Verdadeiro", correct: true },
+      { text: "Falso", correct: false }
+    ]
+  },
+  {
+    question: "O ciclo de vida de um projeto de software geralmente inclui as fases de planejamento, execução e encerramento.",
+    answers: [
+      { text: "Verdadeiro", correct: true },
+      { text: "Falso", correct: false }
+    ]
+  },
+  {
+    question: "A metodologia Agile é conhecida por ser rígida e não adaptável a mudanças durante o desenvolvimento de software.",
+    answers: [
+      { text: "Verdadeiro", correct: false },
+      { text: "Falso", correct: true }
+    ]
+  },
+  {
+    question: "O teste de unidade é um tipo de teste de software onde unidades individuais de código são testadas isoladamente.",
+    answers: [
+      { text: "Verdadeiro", correct: true },
+      { text: "Falso", correct: false }
+    ]
+  },
+  {
+    question: "O modelo em cascata é uma abordagem iterativa para o desenvolvimento de software.",
+    answers: [
+      { text: "Verdadeiro", correct: false },
+      { text: "Falso", correct: true }
+    ]
+  },
+  {
+    question: "O Git é uma ferramenta de controle de versão distribuída criada por Linus Torvalds em 2005.",
+    answers: [
+      { text: "Verdadeiro", correct: true },
+      { text: "Falso", correct: false }
+    ]
+  },
+  {
+    question: "O QA (Quality Assurance) envolve apenas a execução de testes automatizados em software.",
+    answers: [
+      { text: "Verdadeiro", correct: false },
+      { text: "Falso", correct: true }
+    ]
+  },
+  {
+    question: "O Scrum é uma estrutura dentro da metodologia Agile que facilita a colaboração em projetos complexos.",
+    answers: [
+      { text: "Verdadeiro", correct: true },
+      { text: "Falso", correct: false }
+    ]
+  },
+  {
+    question: "O DevOps é uma cultura e prática que visa unificar o desenvolvimento de software (Dev) e a operação de software (Ops).",
+    answers: [
+      { text: "Verdadeiro", correct: true },
+      { text: "Falso", correct: false }
+    ]
+  },
+  {
+    question: "O processo de teste de software nunca deve ser planejado e deve ser sempre improvisado para obter melhores resultados.",
+    answers: [
+      { text: "Verdadeiro", correct: false },
+      { text: "Falso", correct: true }
+    ]
+  }
+];
